ffcb21ff5b677aac588f62843792a374beb3a0f8
Weiner Oliveira <woliveira82@gmail.com>
2024-12-02 11:45:46 +0000

    fix: rocket turn

--- rocket.gd
+++ rocket.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
 const MAX_SPEED := 120.0
-const MANUVER_SPEED := 120.0
+const MANUVER_SPEED := 80.0
 const DOWN_ANGLE := 1.57079637050629
 
 var _speed := MAX_SPEED
 var _active := false
 var _target: Node2D
-var _angle: float = 0.0
+var _max_turn := 2.0
 
 @onready var area_2d: Area2D = $Area2D
 @onready var smoke_particles: CPUParticles2D = $SmokeParticles
@@ -16,24 +16,22 @@ var _angle: float = 0.0
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var destroy_timer: Timer = $DestroyTimer
 
+var target_direction: float
+
 
 func _physics_process(delta: float) -> void:
-	var target_direction: float = (_target.global_position - global_position).angle()
+	target_direction = (_target.global_position - global_position).angle()
 	
 	if _active:
-		var angle_dif = wrapf(target_direction - _angle, -PI, PI)
+		var angle_dif = wrapf(target_direction - rotation, -PI, PI)
 		_speed = MANUVER_SPEED if abs(angle_dif) > 1 else MAX_SPEED
-
-		if abs(angle_dif) > PI:
-			angle_dif = sign(angle_dif) * PI
-
-		rotation += angle_dif * delta
-		_angle = rotation
-
+		
+		rotation += (sign(angle_dif) * _max_turn * delta)
+	
 	else:
-		_angle = DOWN_ANGLE
-
-	position += _speed * Vector2.from_angle(_angle) * delta
+		rotation = DOWN_ANGLE
+	
+	position += _speed * Vector2.from_angle(rotation) * delta
 
 
 func set_target(target: Node2D) -> void:
@@ -49,7 +47,6 @@ func _on_area_2d_area_entered(_area: Area2D) -> void:
 	explosion_particles.emitting = true
 	fragments_particles.emitting = true
 	smoke_particles.emitting = false
-	_active = false
 	area_2d.monitoring = false
 	area_2d.monitorable = false
 	sprite_2d.hide()
--- target.gd
+++ target.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _process(_delta: float) -> void:
-	pass # position = get_viewport().get_mouse_position()
+	position = get_viewport().get_mouse_position()
